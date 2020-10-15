0289503e7b4032b279b61bfe2111ea8f068b6535
EricYXChen <64822411+EricYXChen@users.noreply.github.com>
2020-10-15 00:13:06 -0400

    Testing

--- Tabs.js
+++ Tabs.js
@@ -6,8 +6,8 @@ function openCity(evt, cityName) {
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace("orange", ""); 
+    tablinks[i].className = tablinks[i].className.replace(" w3-red", ""); 
   }
   document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += "orange";
+  evt.currentTarget.className += " w3-red";
 }
